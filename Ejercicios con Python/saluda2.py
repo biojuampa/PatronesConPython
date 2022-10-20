4bfd0b1e82787999461f461a16e4f6ba1e4a8b8d
biojuampa <104794253+biojuampa@users.noreply.github.com>
2022-10-20 18:50:18 -0300

    Ejercicios con Python/saluda2.py

---
+++ Ejercicios con Python/saluda2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def saludar(nombre):
+    print('Hola, %s!' % nombre)
+    
+    
+# Main
+saludar('Juan Pablo')
