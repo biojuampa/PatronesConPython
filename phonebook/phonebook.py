b36bbfcc3e3fe4387e40be34ab50808b79bd39a3
biojuampa <104794253+biojuampa@users.noreply.github.com>
2022-11-11 19:53:38 -0300

    Cambio de ambiente

---
+++ phonebook/phonebook.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  phonebook.py - Intensivo desde Cero (ejercicio 2)
+#
+
+
+import pickle
+import sys
+import os
+from time import sleep
+
+
+phonebook_path = 'phonebook'
+phonebook = {}
+
+
+def create_phonebook(path):
+    print(f'Creando la agenda [{path}] ...')
+    sleep(1)
+    if os.path.exists(f'{path}.bin') and os.path.getsize(f'{path}.bin'):
+        print(f'La agenda [{path}] existe y tiene datos. Por seguridad, no se creará\n'
+              'una agenda nueva que sobreescriba otra existente conteniendo datos.\n'
+              'Primero debe borrarla, bien desde el menú principal, o bien manualmente.\n'
+              )
+        input('Pulse enter para continuar ...')
+        main_menu()
+    
+    global phonebook
+    global phonebook_path
+    try:
+        fhand = open(f'{path}.bin', 'wb')
+        fhand.close()
+        phonebook = {}
+        phonebook_path = path
+    except:
+        print(f'ERROR: No se pudo crear la agenda [{path}]!')
+        sleep(3)
+        main_menu()
+
+
+def delete_phonebook(path):
+    option = input(f'Está seguro que desea borrar permanentemente la agenda [{path}] [y/N]')
+
+    if option == 'y':
+        print(f'Borrando la agenda [{path}] ...')
+        sleep(1)
+        if os.path.exists(f'{path}.bin'):
+            os.remove(f'{path}.bin')
+            global phonebook_path
+            phonebook_path = 'phonebook'
+        else:
+            print(f'La agenda [{path}] NO existe, compruebe el nombre y la ruta.')
+            
+        
+def load_phonebook(path):
+    print(f'Cargando la agenda [{path}] ...')
+    sleep(1)
+    if not os.path.exists(f'{path}.bin'):
+        print(f'La agenda "{path}" no existe!')
+        sleep(3)
+        main_menu()
+    
+    global phonebook_path
+    if not os.path.getsize(f'{path}.bin'):
+        print('La agenda está vacía')
+        phonebook_path = path
+        return
+    
+    global phonebook
+    try:
+        with open(f'{path}.bin', 'rb') as fhand:
+            phonebook = pickle.load(fhand)
+        
+        phonebook_path = path
+    
+    except:
+        print(f'ERROR: Imposible leer la agenda "{path}"!')
+        sleep(3)
+        main_menu()
+
+
+def save_phonebook(path):
+    print(f'Guardando la agenda [{path}] ...')
+    sleep(1)
+    
+    with open(f'{path}.bin', 'wb') as fhand:
+        pickle.dump(phonebook, fhand)
+    
+
+def show_contacts(contact):
+    match = False
+    for name, phones in phonebook.items():
+        if name.startswith(contact):
+            match = True
+            print(f'\n  * {name}:')
+            for i in range(len(phones)):
+                print(f'      - Tel. {i+1}: {phones[i]}')
+    
+    if not match:  
+        print('No hubo coincidencias.')
+        sleep(1)
+    
+    input('\nPersione enter para continuar ...')
+
+
+def add_contact(first, last, numbers):
+    name = f'{last}, {first}'
+    phonebook[name] = numbers
+    save_phonebook(phonebook_path)
+    
+
+def show_all_contacts():
+    for name, phones in phonebook.items():
+        print(f'\n  * {name}:')
+        for i in range(len(phones)):
+            print(f'      - Tel. {i+1}: {phones[i]}')
+    
+    input('\nPersione enter para continuar ...')
+
+
+def add_phone(contact, phone):
+    if contact in phonebook:
+        phonebook[contact].append(phone)
+    else:
+        print(f'ERROR: El nombre de contacto {contact} es incorrecto!')
+        sleep(3)
+
+
+def delete_phone(contact):
+    if not phonebook[contact]:
+        print(f'ERROR: El nombre de contacto {contact} es incorrecto!')
+        return 
+    
+    phones = phonebook[contact]
+    for i in range(len(phones)):
+        print(f'[{i+1}] {phones[i]}')
+    
+    option = input('\nDime qué teléfono deseas borrar: ')
+    
+    print(f'Borrando {phones[int(option)-1]} de {contact} ...')
+    del phones[int(option)-1]
+    sleep(2)
+    
+
+def delete_contact():
+    pass
+
+
+def secondary_menu():
+    while True:
+        os.system('clear')
+        print(f'  AGENDA TELEFÓNICA: [{phonebook_path}]')
+        print('')
+        print('    [1] Mostrar contacto')
+        print('    [2] Agregar contacto')
+        print('    [3] Ver todos los contactos')
+        print('    [4] Agregar teléfono a un contacto')
+        print('    [5] Borrar teléfono de un contacto')
+        print('    [6] Borrar contacto')
+        print('    [7] Guardar contactos')
+        print('    [8] Menú principal')
+        print('')
+        
+        option = input('Elige una opción: ')
+        
+        if option == '1':
+            contact = input('Escribe el nombre o parte de él: ')
+            show_contacts(contact)
+        
+        elif option == '2':
+            first_name = input('Nombre/s del contacto: ')
+            last_name = input('Apellido del contacto: ')
+            phones = list()
+            while True:
+                phone = input(f'Teléfono de {last_name}, {first_name}'
+                                ' (enter para terminar): '
+                                )
+                if not phone:
+                    break
+                
+                phones.append(phone.strip())
+                
+            add_contact(first_name, last_name, phones)
+        
+        elif option == '3':
+            show_all_contacts()
+        
+        elif option == '4':
+            contact = input('Nombre completo del contacto: ')
+            phone = input(f'Teléfono para agregar a {contact}: ')
+            if phone:
+                add_phone(contact, phone)
+        
+        elif option == '5':
+            contact = input('Nombre completo del contacto: ')
+            delete_phone(contact)
+        
+        elif option == '6':
+            pass
+        
+        elif option == '7': # save
+            save_phonebook(phonebook_path)
+        
+        elif option == '8': # main menu
+            save_phonebook(phonebook_path)
+            main_menu()
+        
+        else:
+            pass
+        
+
+# TODO: Opción en el menú principal para listar las agendas creadas
+# TODO: (necesitaré un archivo donde guardar las rutas)
+# TODO: Probablemente me convenga guardar todas las agendas en un mismo directorio
+def main_menu():
+    while True:
+        os.system('clear')
+        print()
+        print('Welcome to the BEST PHONE BOOK in the entire WORLD :-p')
+        print()
+        print('    [1] Abrir agenda existente')
+        print('    [2] Crear agenda nueva')
+        print('    [3] Borrar agenda existente')
+        print('    [4] Salir')
+        print()
+        
+        option = input('Elige una opción: ')
+        
+        if option == '1': # Cargar
+            path = input(f'Ruta y nombre de la agenda [{phonebook_path}]: ')
+            if path:
+                load_phonebook(path)
+            else:
+                load_phonebook(phonebook_path)
+            
+            secondary_menu()
+            
+        elif option == '2': # Crear
+            path = input(f'Ruta y nombre de la agenda [{phonebook_path}]: ')
+            if path:
+                create_phonebook(path)
+            else:
+                create_phonebook(phonebook_path)
+        
+            secondary_menu()
+        
+        elif option == '3': # Borrar
+            path = input(f'Ruta y nombre de la agenda [{phonebook_path}]: ')
+            if path:
+                delete_phonebook(path)
+            else:
+                delete_phonebook(phonebook_path)
+        
+        elif option == '4': # Salir
+            print('\nSee you soon ...\n')
+            sys.exit()
+            
+        else: # Otro
+            pass
+        
+
+main_menu()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
