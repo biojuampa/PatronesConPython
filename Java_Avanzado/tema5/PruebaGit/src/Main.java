f7b1d40f4d775c9d6412d0d40d7adcff350eaab8
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-05-21 17:34:26 -0300

    Nuevo proyecto

---
+++ Java_Avanzado/tema5/PruebaGit/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, al fin pude resolver todo el problema\n" +
+                "que significa usar un entorno gráfico ;)");
+    }
+}
